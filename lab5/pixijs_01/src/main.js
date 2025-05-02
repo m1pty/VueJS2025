40a46a29de8d31e7164a59b690e6e256c038d641
m1pty <109878771+m1pty@users.noreply.github.com>
2025-05-02 23:52:37 +0300

    Created Repo 4 Lab 5, Added PixiJS

---
+++ lab5/pixijs_01/src/main.js
@@ -0,0 +1,28 @@
+// Инициализация приложения PixiJS
+const app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0xc9c9c9
+});
+document.body.appendChild(app.view);
+
+// creating a rectangle
+const rect = new PIXI.Graphics();
+rect.beginFill(0xFF0000);
+
+const rect_width  = 200;
+const rect_height = 100;
+
+rect.drawRect(0, 0, rect_width, rect_height);
+rect.endFill();
+rect.x = (app.screen.width) / 2;
+rect.y = (app.screen.height) / 2;
+rect.pivot.set(
+  rect_width / 2,
+  rect_height/ 2
+);
+app.stage.addChild(rect);
+
+app.ticker.add((delta) => {
+  rect.rotation += 0.01 * delta;
+});
